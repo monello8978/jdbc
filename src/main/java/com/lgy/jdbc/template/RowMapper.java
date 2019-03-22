59e740772195c68743763000f6f0d7b0e91eb9fa
이건영 <monello8978@gmail.com>
2019-03-22 17:40:51 +0900

    소스 추가

---
+++ src/main/java/com/lgy/jdbc/template/RowMapper.java
@@ -0,0 +1,14 @@
+package com.lgy.jdbc.template;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface RowMapper<T> {
+	/**
+	 * resultSet과 오브젝트를 매핑하는 메서드
+	 * @param rs resultSet
+	 * @return resultSet과 매핑한 오브젝트
+	 * @throws SQLException
+	 */
+    T mapRow(ResultSet rs) throws SQLException;
+}
